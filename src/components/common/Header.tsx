49dbcd2ab6f7db063c27389fcdb5ae39490ace7e
myeong-jae-hwi <audwognl@gmail.com>
2025-04-06 09:01:24 +0000

    [Fix] 빌드에러 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,6 @@ import { Globe, Menu, ChevronRight } from 'lucide-react';
 import { useState } from 'react';
 import CornerPattern from './CornerPattern';
 import useOutsideClick from '@/hooks/useOutsideClick';
-import Dropdown from './Dropdown';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -54,8 +53,7 @@ const Header = () => {
         <div className="flex gap-4 text-sm md:text-base">
           <div className="flex items-center gap-2" tabIndex={0}>
             <Globe />
-            {/* 한국어 */}
-            <Dropdown options={['한국어', 'English', '中文']} />
+            한국어
           </div>
           <button
             tabIndex={0}
@@ -78,7 +76,7 @@ const Header = () => {
               </button>
               <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-texture bg-cover bg-no-repeat opacity-10 pointer-events-none" />
               <div className="flex flex-col gap-8 w-full h-full">
-                <div className="relative h-full p-3 sm:p-4 border-2 border-lightBrown bg-lighterBrown">
+                <div className="relative h-full p-4 border-2 border-lightBrown bg-lighterBrown">
                   <CornerPattern borderColor="border-lightBrown" />
                   <nav className="flex flex-col justify-center items-center gap-12 h-full p-4 border-2 border-lightBrown bg-lighterBrown text-base text-navy">
                     <Link href="/map" tabIndex={0}>
@@ -98,7 +96,7 @@ const Header = () => {
                     </Link>
                   </nav>
                 </div>
-                <div className="w-full p-4 sm:p-5 bg-brown">
+                <div className="w-full p-4 bg-brown">
                   <div className="w-full p-20 bg-lightBrown" />
                 </div>
               </div>
