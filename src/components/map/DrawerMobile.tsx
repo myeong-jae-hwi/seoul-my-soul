40f15a81a2cf324dd70639d08a1e242784ac1e55
myeong-jae-hwi <audwognl@gmail.com>
2025-04-06 07:09:05 +0000

    [Comment] 참조 링크 변경

--- src/components/map/DrawerMobile.tsx
+++ src/components/map/DrawerMobile.tsx
@@ -2,21 +2,20 @@ import {
   Drawer,
   DrawerClose,
   DrawerContent,
-  DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger,
 } from '@/components/ui/drawer';
 
 type DrawerMobileProps = {
   marker: Marker;
+  open: boolean;
+  setOpen: (open: boolean) => void;
 };
 
-function DrawerMobile({ marker }: DrawerMobileProps) {
+const DrawerMobile = ({ marker, open, setOpen }: DrawerMobileProps) => {
   return (
-    <Drawer>
-      <DrawerTrigger>Open</DrawerTrigger>
+    <Drawer open={open} onOpenChange={setOpen}>
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>{marker.title}</DrawerTitle>
@@ -24,19 +23,19 @@ function DrawerMobile({ marker }: DrawerMobileProps) {
         <div className="px-4 py-2">
           <img src={`${marker.src}`} alt={`${marker.title}`} className="rounded-xl mb-4" />
           <div className="flex flex-col justify-center gap-3">
-            <p>주소: {marker.location}</p>
-            <p>전화번호: {marker.phone}</p>
-            <p>운영시간: {marker.time}</p>
-            <p>가격: {marker.price}</p>
-            <p>홈페이지: {marker.hompage}</p>
+            <p>주소: {marker.addr}</p>
+            <p>전화번호: {marker.tel_no}</p>
+            <p>운영시간: {marker.operating_time}</p>
+            <p>가격: {marker.entr_fee}</p>
+            <p>홈페이지: {marker.url}</p>
           </div>
         </div>
         <DrawerFooter>
-          <DrawerClose>닫기 </DrawerClose>
+          <DrawerClose>닫기</DrawerClose>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
-}
+};
 
 export default DrawerMobile;
